9cf7f3a2facef3243eb7facb509ab47de5139dfc
NathanPhilliber <nphilliber@gmail.com>
2017-05-21 11:05:02 -0700

    More terrain variation / code formatting

--- Assets/Scripts/TerrainGeneration/LandFrame.cs
+++ Assets/Scripts/TerrainGeneration/LandFrame.cs
@@ -4,117 +4,109 @@ using UnityEngine;
 
 public class LandFrame : MonoBehaviour, IFrame {
 
-	public float xDifference;		//Used for how far away the next vertex will be
-	public float maxYDifference;	//Used for how high/low the next vertex can be from this one
-	public float landDepth;
-	public Material material;
+	public float xDifference;			//	Used for how far away the next vertex will be
+	public float maxYDifference;		//	Used for how high the next vertex can be from this one
+	public float minYDifference;		//	Used for how low the next vertex can be from this one
+	public float landDepth;				//	How deep the land should be
+	public Material material;			//	The material for this piece of land
 
-	private TerrainFrame frame;
-	private EdgeCollider2D collider;
-	private Mesh mesh;
-	private MeshFilter meshFilter;
-	private MeshRenderer meshRenderer;
-
-	// Use this for initialization
-	void Start () {
-		
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	private TerrainFrame frame;			//	The land frame
+	private EdgeCollider2D collider;	//	Collider
+	private Mesh mesh;					//	Land mesh
+	private MeshFilter meshFilter;		//	Mesh filter
+	private MeshRenderer meshRenderer;	//	Renderer
 
 	public void FillFrame(){
-		frame = GetComponent<TerrainFrame> ();
-		mesh = new Mesh ();
-		meshFilter = (MeshFilter)gameObject.AddComponent (typeof(MeshFilter));
-		meshRenderer = (MeshRenderer)gameObject.AddComponent (typeof(MeshRenderer));
-		collider = (EdgeCollider2D)gameObject.AddComponent (typeof(EdgeCollider2D));
-		meshFilter.mesh = mesh;
+		frame = GetComponent<TerrainFrame> ();											//	Assign frame
+		mesh = new Mesh ();																//	New mesh
+		meshFilter = (MeshFilter)gameObject.AddComponent (typeof(MeshFilter));			//	Add mesh filter
+		meshRenderer = (MeshRenderer)gameObject.AddComponent (typeof(MeshRenderer));	//	Add mesh renderer
+		collider = (EdgeCollider2D)gameObject.AddComponent (typeof(EdgeCollider2D));	//	Add edge collider
+		meshFilter.mesh = mesh;															//	Assign mesh to filter
 
-		int impVertices = (int)((frame.width / xDifference) + 3);
-		int middleVertices = (int)((impVertices - 5) / 2);
-		Vector3[] vertices = new Vector3[impVertices + middleVertices];
-		Vector2[] uvs = new Vector2[vertices.Length];
-		Vector2[] points = new Vector2[vertices.Length];
-		int[] triangles = new int[(vertices.Length - 2) * 3];
+		int impVertices = (int)((frame.width / xDifference) + 3);						//	The number of important vertices
+		int middleVertices = (int)((impVertices - 5) / 2);								//	The number of vertices that are used for triangles
+		Vector3[] vertices = new Vector3[impVertices + middleVertices];					//	Vertices array
+		Vector2[] uvs = new Vector2[vertices.Length];									//	UVs array
+		Vector2[] points = new Vector2[vertices.Length];								//	Collider points array
+		int[] triangles = new int[(vertices.Length - 2) * 3];							//	Triangles array
 
-		mesh.Clear ();
+		mesh.Clear ();																	//	Reset mesh
 
+		vertices [0] = new Vector2(0, frame.enterY -landDepth);										//	Start in bottom left corner
+		vertices [1] = new Vector2(0, frame.enterY - transform.position.y);				//	Upper left corner
 
-		vertices [0] = new Vector2(0, -landDepth);
-		vertices [1] = new Vector2(0, frame.enterY - transform.position.y);
+		points[0] = new Vector2(vertices[0].x, vertices[0].y);							//	Bottom left corner for collider point
+		points[1] = new Vector2(vertices[1].x, vertices[1].y);							//	Upper left corner for collider points
 
-		points[0] = new Vector2(vertices[0].x, vertices[0].y);
-		points[1] = new Vector2(vertices[1].x, vertices[1].y);
+		float xCur = 0;																	//	The current x position to place a vertex at
+		float yCur = vertices[1].y;														//	The current y position to place a vertex at
+		float lastRandom = 0;															//	Used to see if we went up or down last time
 
-		float xCur = 0;
-		float yCur = vertices[1].y;
+		for (int i = 2; i < vertices.Length - 1 - middleVertices; i++) {				//	Place all important vertices
+			xCur += xDifference;														//	Add x displacement to xCur
+			float curRandom = Random.Range (minYDifference, maxYDifference);			//	Get a random y offset
+			if (Mathf.Sign (curRandom) != Mathf.Sign (lastRandom) && lastRandom != 0) {	//	If we're going in a different direction than last time
+				curRandom = 0;															//	Then make this y change zero, this prevents sharp points
+			}
 
-		for (int i = 2; i < vertices.Length - 1 - middleVertices; i++) {
-			xCur += xDifference;
-			yCur += Random.Range (-maxYDifference, maxYDifference);
-			vertices [i] = new Vector3 (xCur, yCur, 0);
-			points [i] = new Vector2 (xCur, yCur);
+			yCur += curRandom;															//	Add the y change to the current y
+			lastRandom = curRandom;														//	Save the y change for next iteration
+
+			vertices [i] = new Vector3 (xCur, yCur, 0);									//	Save the vertice
+			points [i] = new Vector2 (xCur, yCur);										//	Save the collider point
 
 		}
 
-		xCur = frame.width;
+		xCur = frame.width;																		//	Start xCur at the end of the frame
 
-		vertices [vertices.Length - 1 - middleVertices] = new Vector3 (xCur, -landDepth, 0);
-		points [vertices.Length - 1 - middleVertices] = new Vector2 (xCur, -landDepth);
+		vertices [vertices.Length - 1 - middleVertices] = new Vector3 (xCur, frame.enterY -landDepth, 0);	//	Place the bottom right vertex
+		points [vertices.Length - 1 - middleVertices] = new Vector2 (xCur,frame.enterY  -landDepth);			//	Place the bottom right collider point
 
 
-		for (int i = vertices.Length - middleVertices; i < vertices.Length; i++) {
-			xCur -= xDifference*2;
-			vertices [i] = new Vector3 (xCur, -landDepth, 0);
-			points [i] = new Vector2 (xCur, -landDepth);
+		for (int i = vertices.Length - middleVertices; i < vertices.Length; i++) {		//	Place all the middle vertices, these are used to create nice triangles
+			xCur -= xDifference*2;														//	Move xCur 2 x displacements to the left
+			vertices [i] = new Vector3 (xCur, frame.enterY -landDepth, 0);							//	Place a vertex along the bottom
+			points [i] = new Vector2 (xCur, frame.enterY -landDepth);								//	Place a collider point along the bottom, this is unnecessary, remove this to optimize
 		}
 
-		//Calculate Triangles
+		int size = vertices.Length;								//	The number of vertices
+		int forwards = 1;										//	Used to keep track of vertex on top of triangle
+		int anchor = 0;											//	Used to keep track of vertex on bottom left of triangle
+		int backwards = -1;										//	Used to keep track of vertex on bottom right of triangle
 
-		int size = vertices.Length;
-		int forwards = 1;
-		int anchor = 0;
-		int backwards = -1;
-
-		for (int i = 0; i < (int)(triangles.Length);) {
-			triangles [i] = anchor;
+		for (int i = 0; i < (int)(triangles.Length);) {			//	Loop through all triangle points
+			triangles [i] = anchor;								//	Create first triangle, 2 points on top, one on bottom
 			triangles [i + 1] = forwards;
 			triangles [i + 2] = ++forwards;
-			//print ("Triangle 1: (" + triangles [i] + ", " + triangles [i + 1] + ", " + triangles [i + 2] + ")");
 			i += 3;
 
-			triangles [i] = anchor;
+			triangles [i] = anchor;								//	Create second triangle, 2 points on top, one on bottom
 			triangles [i + 1] = forwards;
 			triangles [i + 2] = ++forwards;
-			//print ("Triangle 2: (" + triangles [i] + ", " + triangles [i + 1] + ", " + triangles [i + 2] + ")");
 			i += 3;
 
-			triangles [i] = anchor;
+			triangles [i] = anchor;								//	Create third triangle, 1 point on top, two on bottom
 			triangles [i + 1] = forwards;
 			triangles [i + 2] = size + backwards;
-			//print ("Triangle 3: (" + triangles [i] + ", " + triangles [i + 1] + ", " + triangles [i + 2] + ")");
 			i += 3;
 
-			anchor = size + backwards;
-			backwards--;
+			anchor = size + backwards;							//	Move the anchor to the right
+			backwards--;										//	Move the bottom tracker to the right
 
 		}
 
-		for (int i = 0; i < uvs.Length; i++) {
+		for (int i = 0; i < uvs.Length; i++) {						//	Assign all uvs, just copy vertices
 			uvs[i] = new Vector2(vertices[i].x, vertices[i].y);
 		}
-		//print ("Total Triangles: " + (triangles.Length / 3));
-		frame.exitY = yCur + transform.position.y;
 
-		collider.points = points;
-		mesh.vertices = vertices;
-		mesh.uv = uvs;
-		mesh.triangles = triangles;
-		mesh.RecalculateNormals ();
-		meshRenderer.sharedMaterial = material;
+		frame.exitY = yCur + transform.position.y;					//	Save exit position
+
+		collider.points = points;									//	Assign collier points
+		mesh.vertices = vertices;									//	Assign vertices
+		mesh.uv = uvs;												//	Assign uvs
+		mesh.triangles = triangles;									//	Assign triangles
+		mesh.RecalculateNormals ();									//	Calculate the normals with the new vertices
+		meshRenderer.sharedMaterial = material;						//	Set the material
 	}
 }
--- Assets/Scripts/TerrainGeneration/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGeneration/TerrainGenerator.cs
@@ -3,26 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TerrainGenerator : MonoBehaviour {
+	
+	public float levelWidth;					//	How long a level should be (not exact, level will be slightly longer)	
 
-	public float levelWidth;
+	public GameObject[] frames;					//	Prefabs for different frame types
 
-	public GameObject[] frames;
-	// Use this for initialization
 	void Start () {
-		float xCur = transform.position.x;
-		float lastY = 0;
+		float xCur = transform.position.x;		//	Start x counter at the beginning of this frame
+		float lastY = 0;						//	Declare a y tracker, this will set exit points to enter points
 
-		while (xCur < levelWidth + transform.position.x) {
-			GameObject frame = (GameObject)Instantiate (frames [0], new Vector2 (xCur, 0), Quaternion.identity);
-			frame.GetComponent<TerrainFrame> ().enterY = lastY;
-			frame.GetComponent<IFrame> ().FillFrame ();
-			xCur += frame.GetComponent<TerrainFrame>().width;
-			lastY = frame.GetComponent<TerrainFrame> ().exitY;
+		while (xCur < levelWidth + transform.position.x) {															//	Keep going until we are past level width
+			GameObject frame = (GameObject)Instantiate (frames [0], new Vector2 (xCur, 0), Quaternion.identity);	//	Spawn a frame
+			frame.GetComponent<TerrainFrame> ().enterY = lastY;														//	Set the enter point to the last exit point
+			if (frame.GetComponent<LandFrame> () != null) {			//	If this is a land frame
+				float yMax, yMin;
+				if (Random.Range (0, 2) == 0) {						//	50% chance of flat
+					yMax = Random.Range (0f, .2f);
+					yMin = Random.Range (-.2f, 0f);
+				} else {											//	50% chance of getting a chance to be more extreme
+					yMax = Random.Range (0f, 4f);
+					yMin = Random.Range (-4f, 0f);
+				}
+				frame.GetComponent<LandFrame> ().maxYDifference = yMax;
+				frame.GetComponent<LandFrame> ().minYDifference = yMin;
+			}
+			frame.GetComponent<IFrame> ().FillFrame ();																//	Generate frame
+			frame.transform.parent = transform;																		//	Child the frame to this object
+
+			xCur += frame.GetComponent<TerrainFrame>().width;														//	Add frame width to x counter
+			lastY = frame.GetComponent<TerrainFrame> ().exitY;														//	Save the exit point from the new frame
 		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
