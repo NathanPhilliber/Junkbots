e617b823c09ccf058df612f95ee3dbcc38099cb6
Bridget <bmwinn@calpoly.edu>
2017-05-30 19:26:27 -0700

    Editing health bar to receive from Health

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -3,28 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HealthBar : MonoBehaviour {
-	public float MaxHealth, Health;
 	public float scaleY, scaleZ;
+	public Health health;
 
 	// Use this for initialization
 	void Start () {
-		Health = MaxHealth;
 		scaleY = transform.localScale.y;
 		scaleZ = transform.localScale.z;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		// if hit by enemy
-		if ()
-			DecreaseHealth();
-	}
-
-	void DecreaseHealth() {
-		if (Health > 0)
-			Health = Health - 5;
-
-		float scaleX = Health / MaxHealth;
+		float scaleX = health.health / health.maxHealth;
 		transform.localScale = new Vector3 (scaleX, scaleY, scaleZ);
 	}
 }
