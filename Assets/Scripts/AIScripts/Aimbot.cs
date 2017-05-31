14ab5439f592ce7eea271cfc6e393d8b45343072
jerrickcfong <jerrickcfong@gmail.com>
2017-05-30 21:24:45 -0700

    Target an area. Demo contains different ai.

--- Assets/Scripts/AIScripts/Aimbot.cs
+++ Assets/Scripts/AIScripts/Aimbot.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class Aimbot : MonoBehaviour {
 
+	public GameObject Missile;
+	public float speed = 2.0f;
+	private float NextFire;
+	float FireRate = 1.0f;
+	public GameObject target;
+	public float tooFar = 5;
+
 	// Use this for initialization
-	void Start () {
-		
+	new void Start () {
+		// Assumes Layer names are "Bomb" and "AI"
+		Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Bomb"),LayerMask.NameToLayer("AI"));
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+		if (Vector3.Distance (target.transform.position, transform.position) < tooFar && Time.time > NextFire) {
+			NextFire = Time.time + FireRate;
+			shoot();
+		}
+	}
+
+	void shoot() {
+		GameObject missile = Instantiate (Missile, transform.position, Quaternion.identity);
 	}
 }
---
+++ Assets/Scripts/AIScripts/TargetArea.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetArea : MonoBehaviour {
+
+	public Vector3 target;
+	public float tooFar = 5;
+	public float moveSpeed = 3;
+	public string targetTag = "Player";
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.FindWithTag(targetTag).transform.position; //target the player
+
+		Vector3 relPos = target - transform.position;
+		Quaternion rotation = Quaternion.LookRotation (relPos);
+		transform.rotation = rotation;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.forward * Time.deltaTime);
+	}
+}
