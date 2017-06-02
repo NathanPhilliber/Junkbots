9542b327277e82bc07cf2d935258396c70362f39
NathanPhilliber <nphilliber@gmail.com>
2017-06-02 13:12:42 -0700

    Buttons use layers now

--- Assets/Scripts/WorldObjects/PressureButton.cs
+++ Assets/Scripts/WorldObjects/PressureButton.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class PressureButton : MonoBehaviour {
 
 	public GameObject[] objectsToTrigger;	//	The things that are affected by this button
-	public string tagTrigger;				//	Objects with this tag can interact with this button
+	public LayerMask mask;				//	Objects with this layermask can interact with this button
 	public bool toggle;
 
 	/*
 	 * Called when this button is pressed
 	 */
 	void OnTriggerEnter2D(Collider2D other){
-		if (toggle == true && other.CompareTag (tagTrigger)) {
+		if (toggle == true && mask == (mask | (1 << other.gameObject.layer))) {
 			for (int i = 0; i < objectsToTrigger.Length; i++) {
 				objectsToTrigger [i].GetComponent<IInteractable>().TriggerAction (true);
 			}
@@ -21,7 +21,7 @@ public class PressureButton : MonoBehaviour {
 	}
 
 	void OnTriggerStay2D(Collider2D other){
-		if (toggle == false && other.CompareTag (tagTrigger)) {
+		if (toggle == false && mask == (mask | (1 << other.gameObject.layer))) {
 			for (int i = 0; i < objectsToTrigger.Length; i++) {
 				objectsToTrigger [i].GetComponent<IInteractable>().TriggerAction (false);
 			}
