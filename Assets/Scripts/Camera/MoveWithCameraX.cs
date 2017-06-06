8959fd53f98910d1f1764dc77f28d0e7b49b183f
NathanPhilliber <nphilliber@gmail.com>
2017-06-06 16:26:20 -0700

    Sound changes and level select screen

---
+++ Assets/Scripts/Camera/MoveWithCameraX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithCameraX : MonoBehaviour {
+
+	public float perXUnit;
+
+	public Transform pivot;
+
+
+
+	void Start () {
+		
+		lastX = pivot.position.x;
+	}
+	
+
+	private float lastX;
+	void Update () {
+		float delX = pivot.position.x - lastX;
+		transform.Translate (new Vector2(delX * perXUnit, 0));
+		lastX = pivot.position.x;
+	}
+}
