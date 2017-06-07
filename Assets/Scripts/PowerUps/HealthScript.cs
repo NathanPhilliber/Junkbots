1b0034072fbb2e2aed1efa3600563f1f422a6687
Bridget <bmwinn@calpoly.edu>
2017-06-07 01:13:08 -0700

    Created three powerups

---
+++ Assets/Scripts/PowerUps/HealthScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour {
+
+	public float healthBoost;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(BoxCollider2D other) {
+		if (other.name == "Isa" || other.name == "Erl") {
+			Destroy (gameObject, 0.5F);
+		}
+	}
+}
---
+++ Assets/Scripts/PowerUps/TimedPowerUp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedPowerUp : MonoBehaviour {
+	public bool isActive;
+	public float activeTimer;
+
+	// Use this for initialization
+	void Start () {
+		isActive = false;
+		activeTimer = 10.0F;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (isActive && activeTimer > 0) {
+			activeTimer -= Time.deltaTime;
+		}
+	}
+
+	void OnTriggerEvent2D(Collider2D other) {
+		if (other.name == "Isa" || other.name == "Erl") {
+			isActive = true;
+			Destroy (gameObject, 0.5F);
+		}
+	}
+}
