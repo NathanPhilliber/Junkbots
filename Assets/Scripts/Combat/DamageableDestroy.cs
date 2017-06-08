e680ef003fab3d5cb1fe6a9381c17fa3f4391145
NathanPhilliber <nphilliber@gmail.com>
2017-06-08 11:09:48 -0700

    Level 1 ending

--- Assets/Scripts/Combat/DamageableDestroy.cs
+++ Assets/Scripts/Combat/DamageableDestroy.cs
@@ -4,17 +4,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DamageableDestroy : Damageable {
-    public GameObject deathEffect;
+    public GameObject deathEffect, damageEffect;
+	public float damageRotation;
 
     public override void OnHealthDecreased(int amount)
     {
-        // Nothing yet
+		if (damageEffect != null) {
+			GameObject effect = (GameObject)Instantiate (damageEffect, new Vector2 (transform.position.x, transform.position.y), Quaternion.Euler(0,0, damageRotation));
+			Destroy (effect, 1);
+		}
     }
 
     public override void OnHealthZero()
     {
         if (deathEffect != null)
-            GameObject.Instantiate(deathEffect, transform.position, Quaternion.identity);
+			GameObject.Instantiate(deathEffect, new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
         Destroy(gameObject);
     }
 
