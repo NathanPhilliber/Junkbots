bdba042f6e12cdf4edf3c22f6e65f577d1bc68eb
Bridget <bmwinn@calpoly.edu>
2017-06-08 23:24:36 -0700

    Implementing Moar's death

---
+++ Assets/Scripts/Enemies/MoarDies.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoarDies : MonoBehaviour, IInteractable {
+
+	public GameObject[] explosions;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void TriggerAction(bool toggle) {
+		if (toggle) {
+			for (int i = 0; i < explosions.Length; i++) {
+				
+			}
+		}
+	}
+}
